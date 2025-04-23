d7eeaa09472d2f9d8673d0298adb91326e408548
what-markeart <what-markeart@gmail.com>
2025-04-23 13:22:48 +0100

    Refactor service.html: Update contact information and improve layout
    - Adjusted the navbar toggler button for better readability.
    - Updated contact details including phone number and email address.
    - Replaced placeholder text with actual location and contact information.
    - Added TikTok icon and link, replacing the Twitter icon.
    - Enhanced service description for clarity and impact.
    - Cleaned up commented-out code for better maintainability.

--- js/custom.js
+++ js/custom.js
@@ -21,26 +21,30 @@ $(function () {
 });
 
 // owl carousel slider js
-$('.team_carousel').owlCarousel({
-    loop: true,
-    margin: 15,
-    dots: true,
-    autoplay: true,
-    navText: [
-        '<i class="fa fa-angle-left" aria-hidden="true"></i>',
-        '<i class="fa fa-angle-right" aria-hidden="true"></i>'
-    ],
-    autoplayHoverPause: true,
-    responsive: {
-        0: {
-            items: 1,
-            margin: 0
-        },
-        576: {
-            items: 2,
-        },
-        992: {
-            items: 3
+if (typeof $.fn.owlCarousel !== 'undefined') {
+    $('.team_carousel').owlCarousel({
+        loop: true,
+        margin: 15,
+        dots: true,
+        autoplay: true,
+        navText: [
+            '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+            '<i class="fa fa-angle-right" aria-hidden="true"></i>'
+        ],
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1,
+                margin: 0
+            },
+            576: {
+                items: 2,
+            },
+            992: {
+                items: 3
+            }
         }
-    }
-})
+    });
+} else {
+    console.error('Owl Carousel library is not loaded.');
+}
